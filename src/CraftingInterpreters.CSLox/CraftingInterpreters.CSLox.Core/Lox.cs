eaff62e9888c64168af92435332b91bd73c2c3f9
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-10-29 17:26:58 +0400

    Develop the full Lox language scanner

--- src/CraftingInterpreters.CSLox/CraftingInterpreters.CSLox.Core/Lox.cs
+++ src/CraftingInterpreters.CSLox/CraftingInterpreters.CSLox.Core/Lox.cs
@@ -1,71 +1,9 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
 
 namespace CraftingInterpreters.CSLox.Core
 {
-	public class Lox
-	{
-
-		static bool _hadError = false;
-
-		public static void Main(string[] args)
-		{
-			if (args.Length > 1)
-			{
-				Console.WriteLine("Usage: cslox [script]");
-				Environment.Exit(64);
-			}
-			else if (args.Length == 1)
-			{
-				RunFile(args[0]);
-			}
-			else
-			{
-				RunPrompt();
-			}
-		}
-
-		private static void RunFile(string path)
-		{
-			byte[] bytes = File.ReadAllBytes(path);
-			Run(Encoding.UTF8.GetString(bytes));
-
-			// Indicate an error in the exit code.
-			if (_hadError)
-				Environment.Exit(65);
-		}
-
-		private static void RunPrompt()
-		{
-			for (; ; )
-			{
-				Console.Write("> ");
-				string? line = Console.ReadLine();
-				if (line == null) break;
-				Run(line);
-				_hadError = false;
-			}
-		}
-
-		private static void Run(string source)
-		{
-			var scanner = new Scanner(source);
-			var tokens = new
-
-		}
-
-		static void Error(int line, string message)
-		{
-			Report(line, "", message);
-		}
-
-		private static void Report(int line, string where, string message)
-		{
-			Console.WriteLine($"[Line: {line}] Error {where}: {message}");
-			_hadError = true;
-		}
-
-	}
-
 	public class Scanner
 	{
 		private readonly string _source;
@@ -79,6 +17,27 @@ namespace CraftingInterpreters.CSLox.Core
 		private int _start = 0;
 		private int _current = 0;
 		private int _line = 1;
+		private static readonly Dictionary<string, TokenType> _keywords = new()
+		{
+			{ "and", TokenType.AND },
+			{ "or", TokenType.OR },
+			{ "var", TokenType.VAR },
+			{ "fun", TokenType.FUN },
+			{ "if", TokenType.IF },
+			{ "else", TokenType.ELSE },
+			{ "class", TokenType.CLASS },
+			{ "nil", TokenType.NIL },
+			{ "print", TokenType.PRINT },
+			{ "super", TokenType.SUPER },
+			{ "return", TokenType.RETURN },
+			{ "this", TokenType.THIS },
+			{ "true", TokenType.TRUE },
+			{ "false", TokenType.FALSE },
+			{ "for", TokenType.FOR },
+			{ "while", TokenType.WHILE },
+
+		};
+
 
 		/// <summary>
 		/// Analyze the source code and extract a list of tokens.
@@ -89,7 +48,7 @@ namespace CraftingInterpreters.CSLox.Core
 			while (!IsAtEnd())
 			{
 				_start = _current;
-				ScanTokens();
+				ScanToken();
 			}
 
 			_tokens.Add(new Token(TokenType.EOF, "", null, _line));
@@ -119,9 +78,6 @@ namespace CraftingInterpreters.CSLox.Core
 				case '}':
 					AddToken(TokenType.RIGHT_BRACE);
 					break;
-				case '/':
-					AddToken(TokenType.SLASH);
-					break;
 				case '.':
 					AddToken(TokenType.DOT);
 					break;
@@ -134,23 +90,122 @@ namespace CraftingInterpreters.CSLox.Core
 				case ',':
 					AddToken(TokenType.COMMA);
 					break;
+				case '!':
+					AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+					break;
+				case '=':
+					AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+					break;
+				case '<':
+					AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.EQUAL);
+					break;
+				case '>':
+					AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.EQUAL);
+					break;
+				case '/':
+					// Check if it matches a comment with two slashes, so keep moving until the end of the line
+					if (Match('/'))
+					{
+						while (Peek() != '\n' && !IsAtEnd())
+						{
+							Advance();
+						}
+					}
+					else
+						AddToken(TokenType.SLASH);
+					break;
+
+				// Skip and process the useless characters 
+				case ' ':
+				case '\t':
+				case '\r':
+					break;
+
+				// Handle new lines
+				case '\n':
+					_line++;
+					break;
+
+				// Handle String Literals
+				case '"':
+					HandleStringToken();
+					break;
+				default:
+					if (IsDigit(c))
+						HandleNumberToken();
+					else if (IsAlpha(c))
+						HandleIdentifierToken();
+					else
+						Lox.Error(_line, "Unexpected character.");
+					break;
 			}
 		}
 
-		private char Advance()
+		/// <summary>
+		/// Process a literal string token 
+		/// </summary>
+		private void HandleStringToken()
 		{
-			return _source[_current++];
+			// Keep moving until reaching the close "
+			while (Peek() != '"' && !IsAtEnd())
+			{
+				if (Peek() == '\n')
+					_line++;
+				Advance();
+			}
+
+			// Check if the current source file ends before reading the closing "
+			if (IsAtEnd())
+			{
+				Lox.Error(_line, "Unterminated string");
+				return;
+			}
+
+			// Move one step forward to jump on from the closing "
+			Advance();
+
+			// Read the string value from the start position up to the current position and skip the open and closing "
+			var stringValue = _source[(_start + 1)..(_current - 1)];
+			AddToken(TokenType.STRING, stringValue);
 		}
 
-		private void AddToken(TokenType type)
+		/// <summary>
+		/// Process a literal number token
+		/// </summary>
+		private void HandleNumberToken()
 		{
-			AddToken(type, null);
+			while (IsDigit(Peek()))
+				Advance();
+
+			if (Peek() == '.' && IsDigit(PeekNext()))
+			{
+				while (IsDigit(Peek()))
+					Advance();
+			}
+
+			var stringValue = _source[_start.._current];
+			var doubleValue = double.Parse(stringValue);
+			AddToken(TokenType.NUMBER, doubleValue);
 		}
 
-		private void AddToken(TokenType type, object? literal)
+		private void HandleIdentifierToken()
 		{
-			_tokens.Add(new(type, _source[_start.._current], literal, _line));
+			while (IsAlphaNumeric(Peek()))
+				Advance();
+
+			var stringValue = _source[_start.._current];
+			var isKeyword = _keywords.TryGetValue(stringValue, out var type);
+			if (!isKeyword)
+				type = TokenType.IDENTIFIER;
+
+			AddToken(type);
 		}
+
+		/// <summary>
+		/// Determine if the current character is a digit or no
+		/// </summary>
+		/// <returns></returns>
+		private bool IsDigit(char c) => c >= '0' && c <= '9';
 
 		/// <summary>
 		/// Determine if the current cursor is at the end of the source code.
@@ -158,6 +213,95 @@ namespace CraftingInterpreters.CSLox.Core
 		/// <returns></returns>
 		private bool IsAtEnd()
 			=> _current >= _source.Length;
+
+		/// <summary>
+		/// Read the upcoming character and move the cursor a single char forward
+		/// </summary>
+		/// <returns></returns>
+		private char Advance()
+		{
+			return _source[_current++];
+		}
+
+		/// <summary>
+		/// Read the current character without moving the cursor a single char forward
+		/// </summary>
+		/// <returns></returns>
+		private char Peek()
+		{
+			if (IsAtEnd())
+				return '\0';
+
+			return _source[_current];
+		}
+
+		/// <summary>
+		/// Read the next character without moving the cursor a single char forward
+		/// </summary>
+		/// <returns></returns>
+		private char PeekNext()
+		{
+			if (_current + 1 >= _source.Length)
+				return '\0';
+
+			return _source[_current + 1];
+		}
+		/// <summary>
+		/// Peek at the upcoming character and check if it matches an expected value, if it does move the cursor forward, otherwise leave it where it is.
+		/// </summary>
+		/// <param name="expected"></param>
+		/// <returns></returns>
+		private bool Match(char expected)
+		{
+			if (IsAtEnd())
+				return false;
+
+			if (_source[_current] == expected)
+			{
+				_current++;
+				return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Add a token to the list of tokens with no literal value
+		/// </summary>
+		/// <param name="type"></param>
+		private void AddToken(TokenType type)
+		{
+			AddToken(type, null);
+		}
+
+		/// <summary>
+		/// Add a token to the list of tokens with a literal value
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="literal"></param>
+		private void AddToken(TokenType type, object? literal)
+		{
+			_tokens.Add(new(type, _source[_start.._current], literal, _line));
+		}
+
+		/// <summary>
+		/// Determine if a character is a alphabetical character or an underscore 
+		/// </summary>
+		/// <param name="c"></param>
+		/// <returns></returns>
+		private bool IsAlpha(char c)
+		{
+			return c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c == '_';
+		}
+
+		/// <summary>
+		/// Determine if a character is an alphanumerical
+		/// </summary>
+		/// <param name="c"></param>
+		/// <returns></returns>
+		private bool IsAlphaNumeric(char c)
+			=> IsDigit(c) || IsAlpha(c);
+
 	}
 
 	public class Token
@@ -188,7 +332,7 @@ namespace CraftingInterpreters.CSLox.Core
 		LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
 		// One or two character tokens.
-		BANG, BANG_EQUAL, EQUAL, EQUAL_EUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+		BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
 
 		// Literals
 		IDENTIFIER, STRING, NUMBER,
