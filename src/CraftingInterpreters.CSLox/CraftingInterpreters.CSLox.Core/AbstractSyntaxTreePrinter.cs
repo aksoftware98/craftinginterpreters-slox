876c7f43923c331c5903bb2055211a83a56df23a
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-11-02 04:38:04 +0400

    Implement the PrintVisitor to try to print a try

---
+++ src/CraftingInterpreters.CSLox/CraftingInterpreters.CSLox.Core/AbstractSyntaxTreePrinter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftingInterpreters.CSLox.Core;
+
+public class AbstractSyntaxTreePrinter : IVisitor<string>
+{
+	public string VisitBinaryLoxExpression(BinaryLoxExpression loxExpression)
+		=> Parenthesize(loxExpression.Operator.Lexeme, loxExpression.Left, loxExpression.Right);
+
+	public string VisitGroupingLoxExpression(GroupingLoxExpression loxExpression)
+		=> Parenthesize("group", loxExpression.Expression);
+
+	public string VisitLiteralLoxExpression(LiteralLoxExpression loxExpression)
+		=> loxExpression.Value?.ToString() ?? "nil";
+
+	public string VisitUnaryLoxExpression(UnaryLoxExpression loxExpression)
+		=> Parenthesize(loxExpression.Operator.Lexeme, loxExpression.Right);
+
+	public string Print(LoxExpression expression)
+	{
+		return expression.Accept(this);
+	}
+
+	private string Parenthesize(string name, params LoxExpression[] expressions)
+	{
+		var builder = new StringBuilder();
+
+		builder.Append($"(").Append(name);
+		foreach ( var expression in expressions )
+		{
+			builder.Append(" ");
+			builder.Append(expression.Accept(this));
+		}
+		builder.Append(")");
+
+		return builder.ToString();
+	}
+}
