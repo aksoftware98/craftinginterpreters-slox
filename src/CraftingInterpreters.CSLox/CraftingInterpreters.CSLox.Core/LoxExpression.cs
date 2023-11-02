53f79f11722d8b050512dc946fbccad655057018
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-11-02 04:19:55 +0400

    Add the VisitorPattern to the generated expressions

--- src/CraftingInterpreters.CSLox/CraftingInterpreters.CSLox.Core/LoxExpression.cs
+++ src/CraftingInterpreters.CSLox/CraftingInterpreters.CSLox.Core/LoxExpression.cs
@@ -12,55 +12,85 @@ namespace CraftingInterpreters.CSLox.Core;
 
 public abstract class LoxExpression
 {
-	public class Binary : LoxExpression
+	public abstract T Accept<T>(IVisitor<T> visitor);
+}
+
+public interface IVisitor<T>
+{
+	T VisitBinaryLoxExpression(BinaryLoxExpression loxExpression);
+	T VisitGroupingLoxExpression(GroupingLoxExpression loxExpression);
+	T VisitLiteralLoxExpression(LiteralLoxExpression loxExpression);
+	T VisitUnaryLoxExpression(UnaryLoxExpression loxExpression);
+}
+
+public class BinaryLoxExpression : LoxExpression
+{
+	public BinaryLoxExpression(LoxExpression left, Token @operator, LoxExpression right)
 	{
-		public Binary(LoxExpression left, Token @operator, LoxExpression right)
-		{
-			this.Left = left;
-			this.Operator = @operator;
-			this.Right = right;
-		}
-
-		public LoxExpression Left { get; set; }
-		public Token Operator { get; set; }
-		public LoxExpression Right { get; set; }
-
+		this.Left = left;
+		this.Operator = @operator;
+		this.Right = right;
 	}
 
-	public class Grouping : LoxExpression
+	public override T Accept<T>(IVisitor<T> visitor)
 	{
-		public Grouping(LoxExpression expression)
-		{
-			this.Expression = expression;
-		}
-
-		public LoxExpression Expression { get; set; }
-
+		return visitor.VisitBinaryLoxExpression(this);
 	}
 
-	public class Literal : LoxExpression
-	{
-		public Literal(object value)
-		{
-			this.Value = value;
-		}
-
-		public object Value { get; set; }
-
-	}
-
-	public class Unary : LoxExpression
-	{
-		public Unary(Token @operator, LoxExpression right)
-		{
-			this.Operator = @operator;
-			this.Right = right;
-		}
-
-		public Token Operator { get; set; }
-		public LoxExpression Right { get; set; }
-
-	}
+	public LoxExpression Left { get; set; }
+	public Token Operator { get; set; }
+	public LoxExpression Right { get; set; }
 
 }
+
+public class GroupingLoxExpression : LoxExpression
+{
+	public GroupingLoxExpression(LoxExpression expression)
+	{
+		this.Expression = expression;
+	}
+
+	public override T Accept<T>(IVisitor<T> visitor)
+	{
+		return visitor.VisitGroupingLoxExpression(this);
+	}
+
+	public LoxExpression Expression { get; set; }
+
+}
+
+public class LiteralLoxExpression : LoxExpression
+{
+	public LiteralLoxExpression(object value)
+	{
+		this.Value = value;
+	}
+
+	public override T Accept<T>(IVisitor<T> visitor)
+	{
+		return visitor.VisitLiteralLoxExpression(this);
+	}
+
+	public object Value { get; set; }
+
+}
+
+public class UnaryLoxExpression : LoxExpression
+{
+	public UnaryLoxExpression(Token @operator, LoxExpression right)
+	{
+		this.Operator = @operator;
+		this.Right = right;
+	}
+
+	public override T Accept<T>(IVisitor<T> visitor)
+	{
+		return visitor.VisitUnaryLoxExpression(this);
+	}
+
+	public Token Operator { get; set; }
+	public LoxExpression Right { get; set; }
+
+}
+
 
--- src/CraftingInterpreters.CSLox/CraftingInterpreters.CSLox.Tools/Program.cs
+++ src/CraftingInterpreters.CSLox/CraftingInterpreters.CSLox.Tools/Program.cs
@@ -34,6 +34,14 @@ static void DefineAst(string outputDir, string baseName, List<string> types)
 	writer.WriteLine();
 	writer.WriteLine($"public abstract class {baseName}");
 	writer.WriteLine("{");
+	// Define the abstract accept method for the visitor pattern
+	writer.WriteLine($"\tpublic abstract T Accept<T>(IVisitor<T> visitor);");
+
+	writer.WriteLine("}");
+
+	writer.WriteLine();
+	// Define the visitor interface
+	DefineVisitor(writer, baseName, types);
 
 	foreach (var type in types)
 	{
@@ -42,7 +50,6 @@ static void DefineAst(string outputDir, string baseName, List<string> types)
 		DefineType(writer, baseName, className, fields);
 	}
 
-	writer.WriteLine("}");
 	writer.WriteLine();
 
 	writer.Dispose();
@@ -51,11 +58,11 @@ static void DefineAst(string outputDir, string baseName, List<string> types)
 
 static void DefineType(StreamWriter writer, string baseName, string className, string fields)
 {
-	writer.WriteLine($"\tpublic class {className} : {baseName}");
-	writer.WriteLine("\t{");
+	writer.WriteLine($"public class {className}LoxExpression : {baseName}");
+	writer.WriteLine("{");
 	// Constructor
-	writer.WriteLine($"\t\tpublic {className}({fields})");
-	writer.WriteLine("\t\t{");
+	writer.WriteLine($"\tpublic {className}{baseName}({fields})");
+	writer.WriteLine("\t{");
 
 	// Define the fields
 	var fieldList = fields.Split(", ");
@@ -63,10 +70,18 @@ static void DefineType(StreamWriter writer, string baseName, string className, s
 	{
 		var name = field.Split(" ")[1];
 		var propertyName = GetPropertyName(name);
-		writer.WriteLine($"\t\t\tthis.{propertyName} = {name};");
+		writer.WriteLine($"\t\tthis.{propertyName} = {name};");
 	}
 
-	writer.WriteLine("\t\t}");
+	writer.WriteLine("\t}");
+
+	// Define the visitor pattern Accept method
+	writer.WriteLine();
+	writer.WriteLine($"\tpublic override T Accept<T>(IVisitor<T> visitor)");
+	writer.WriteLine("\t{");
+	writer.WriteLine($"\t\treturn visitor.Visit{GetPropertyName(className)}{baseName}(this);");
+	writer.WriteLine("\t}");
+
 	writer.WriteLine();
 
 	// Define the properties
@@ -75,14 +90,34 @@ static void DefineType(StreamWriter writer, string baseName, string className, s
 		var type = field.Split(" ")[0];
 		var name = field.Split(" ")[1];
 		name = GetPropertyName(name);
-		writer.WriteLine($"\t\tpublic {type} {name} {{ get; set; }}");
+		writer.WriteLine($"\tpublic {type} {name} {{ get; set; }}");
 	}
 
 	writer.WriteLine();
-	writer.WriteLine("\t}");
+	writer.WriteLine("}");
 	writer.WriteLine();
 }
 
+/// <summary>
+/// Define the interface related to the visitor pattern
+/// </summary>
+static void DefineVisitor(StreamWriter writer, string baseName, List<string> types)
+{
+	writer.WriteLine($"public interface IVisitor<T>");
+	writer.WriteLine("{");
+	foreach (var item in types)
+	{
+		var typeName = item.Split(":")[0].Trim();
+		var methodLine = $"\tT Visit{GetPropertyName(typeName)}{baseName}({typeName}{baseName} loxExpression);";
+		writer.WriteLine(methodLine);
+	}
+	writer.WriteLine("}");
+	writer.WriteLine();
+}
+
+/// <summary>
+/// Capitalize the first letter in a name
+/// </summary>
 static string GetPropertyName(string field)
 {
 	if (field.StartsWith("@"))
